047a66e80ab2a9d9fd2a4761d0ae28c8a4b5cd31
Daniel Micevski <dmicevski@v3.org.au>
2015-02-25 13:40:19 +1100

    disables member profile parameters - citizenship, residency, ethnicity/race/hispanic.

---
+++ patches/plugins-members-profile-views-index-tmpl-default.php
@@ -0,0 +1,16 @@
+791c791
+< 		<?php if ($this->registration->Citizenship != REG_HIDE) : ?>
+---
+> 		<?php if ($this->registration->Citizenship != REG_HIDE && false) : ?>
+890c890
+< 		<?php if ($this->registration->Residency != REG_HIDE) : ?>
+---
+> 		<?php if ($this->registration->Residency != REG_HIDE && false) : ?>
+1202c1202
+< 		<?php if ($this->registration->Hispanic != REG_HIDE) : ?>
+---
+> 		<?php if ($this->registration->Hispanic != REG_HIDE && false) : ?>
+1326c1326
+< 		<?php if ($this->registration->Race != REG_HIDE) : ?>
+---
+> 		<?php if ($this->registration->Race != REG_HIDE && false) : ?>
